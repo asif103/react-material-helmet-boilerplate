cdcc4f59c0151ce6ebb3dd7dc5ed2392ec8d626c
Mohammed Asif <asif.cse473@gmail.com>
2023-11-03 18:56:36 +0600

    installed material ui and react helmet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "@mui/material";
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Title from helmet</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +25,7 @@ function App() {
           Learn React
         </a>
       </header>
+        <Button variant={'contained'}>Matrial UI installed successfully!</Button>
     </div>
   );
 }
